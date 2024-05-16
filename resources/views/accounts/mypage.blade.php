a12ad77003214cf3e450605459e8f0f908230d43
八木大介 <yagi.d@inlife.work>
2024-05-16 22:46:51 +0900

    ページ仮構成

--- resources/views/accounts/mypage.blade.php
+++ resources/views/accounts/mypage.blade.php
@@ -3,7 +3,18 @@
 @section('content')
 <section class="container">
     <div class="row m-auto">
-        <h1>マイページ</h1>
+        <div class="col-6 mx-auto">
+            <h1>マイページ</h1>
+            <img src="{{ asset('images/logo.png') }}" alt="ユーザー画像">
+            <p>ユーザー名</p>
+            <p>会員ランク</p>
+            <p>ユーザーID</p>
+            <p>フォロー数</p>
+            <p>フォロワー数</p>
+            <h2>目的</h2>
+            <p>ペットの写真を投稿する</p>
+            <p>ペットの記事を投稿する</p>
+        </div>
     </div><!-- /.row -->
 </section>
 @endsection
